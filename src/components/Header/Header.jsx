f0e2f0d5b2bf51e5a86ffc4b08cab6b81d2ef2ef
rio <riojordan127@yahoo.com>
2025-08-03 18:56:27 -0500

    add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.png";
+
+function Header() {
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} />
+      <p className="header__date-and-location">Date, Location</p>
+      <button className="header__add-clothes-btn">+ Add Clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">Rio Morales</p>
+        <img src={avatar} alt="Rio Morales" className="header__avatar" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
