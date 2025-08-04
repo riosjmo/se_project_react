aed54e46097a9494a90fa3621f62568845be7f6c
rio <riojordan127@yahoo.com>
2025-08-04 16:27:54 -0500

    add cards

---
+++ src/components/ItemCard/ItemCard.jsx
@@ -0,0 +1,13 @@
+import "./ItemCard.css";
+import "../Main/Main.css";
+
+function ItemCard({ item }) {
+  return (
+    <div className="item-card">
+      <h2 className="item-card__name">{item.name}</h2>
+      <img className="item-card__image" src={item.link} alt={item.name} />
+    </div>
+  );
+}
+
+export default ItemCard;
