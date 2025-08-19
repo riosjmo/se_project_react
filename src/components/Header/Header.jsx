87154637c0774466301067837a4ea44ed2b06041
rio <riojordan127@yahoo.com>
2025-08-18 19:09:45 -0500

    add link navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
@@ -12,7 +13,9 @@ function Header({ handleAddClick, weatherData }) {
   return (
     <header className="header">
       <div className="header__left">
-        <img className="header__logo" alt="logo" src={logo} />
+        <Link to="/">
+          <img className="header__logo" alt="logo" src={logo} />
+        </Link>
         <p className="header__date-and-location">
           {currentDate}, {weatherData.city}
         </p>
@@ -27,10 +30,12 @@ function Header({ handleAddClick, weatherData }) {
         >
           + Add Clothes
         </button>
-        <div className="header__user-container">
-          <p className="header__username">Terrence Tegegne</p>
-          <img src={avatar} alt="Rio Morales" className="header__avatar" />
-        </div>
+        <Link to="/profile" className="header__link"> 
+          <div className="header__user-container">
+            <p className="header__username">Terrence Tegegne</p>
+            <img src={avatar} alt="Rio Morales" className="header__avatar" />
+          </div>
+        </Link>
       </div>
     </header>
   );
